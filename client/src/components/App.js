b8ad06d9736ff2234522e33ac9e6b5b1b1452205
fahim-arif <fahim1.618555@gmail.com>
2021-04-09 07:02:17 +0600

    final commit ready

---
+++ client/src/components/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList}></Route>
+            <Route path="/streams/new" exact component={StreamCreate}></Route>
+            <Route
+              path="/streams/edit/:id"
+              exact
+              component={StreamEdit}
+            ></Route>
+            <Route
+              path="/streams/delete/:id"
+              exact
+              component={StreamDelete}
+            ></Route>
+            <Route path="/streams/:id" exact component={StreamShow}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+export default App;
---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,20 @@
+import React from "react";
+import StreamForm from "./StreamForm";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
+
+class StreamCreate extends React.Component {
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
+  render() {
+    return (
+      <div>
+        <h3>Create A Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
+    );
+  }
+}
+
+export default connect(null, { createStream })(StreamCreate);
